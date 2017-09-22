f2af17d9041b6b3a068e74987444301161b8014e
peter <hfutqiufei@163.com>
2017-09-22 17:29:46 +0800

    better everyday!

---
+++ README.md
@@ -0,0 +1,8 @@
+# 我的各种发言
+
+
+
+[测试xargin](http://htmlpreview.github.com/?https://github.com/qiufei/banking/blob/master/PPT/第一章导论.html)
+
+
+
---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+cd .
+
+git pull
+
+git add --all
+
+git commit -a -m " better everyday!"
+
+git push
+
+# finally
+echo "presentations数据已经更新到github！"
